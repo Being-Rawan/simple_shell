993a8602d267a8a91f140d42ccdc04351d06d8d7
shahdshaaban <shabanshahd634@gmail.com>
2023-07-26 11:59:19 -0700

    updated

--- history.c
+++ history.c
@@ -64,88 +64,49 @@ int write_history(info_t *info)
 
 int read_history(info_t *info)
 {
+	int i, last = 0, linecount = 0;
+	ssize_t fd, rdlen, fsize = 0;
+	struct stat st;
+	char *buf = NULL, *filename = NULL;
+
 	if (!info)
-		return 0;
-
-	char* filename = get_history_file(info);
+		return (0);
+	filename = get_history_file(info);
 	if (!filename)
-		return 0;
-
-	int fd = open(filename, O_RDONLY);
+		return (0);
+	fd = open(filename, O_RDONLY);
 	free(filename);
 	if (fd == -1)
-		return 0;
-
-	struct stat st;
-	if (fstat(fd, &st) != 0)
-	{
-		close(fd);
-		return 0;
-	}
-
-	ssize_t fsize = st.st_size;
+		return (0);
+	if (fstat(fd, &st) == 0)
+		fsize = st.st_size;
 	if (fsize < 2)
-	{
-		close(fd);
-		return 0;
-	}
-
-	char* buf = read_file_contents(fd, fsize);
-	close(fd);
-
+		return (close(fd), 0);
+	buf = malloc(sizeof(char) * (fsize + 1));
 	if (!buf)
-		return 0;
-
-	int linecount = process_file_contents(buf, fsize, info);
-	free(buf);
-
-	return linecount;
-}
-
-char* read_file_contents(int fd, ssize_t fsize)
-{
-	char* buf = malloc(sizeof(char) * (fsize + 1));
-	if (!buf)
-		return NULL;
-
-	ssize_t rdlen = read(fd, buf, fsize);
-	buf[fsize] = '\0';
-
+		return (close(fd), 0);
+	rdlen = read(fd, buf, fsize);
+	buf[fsize] = 0;
 	if (rdlen <= 0)
-	{
-		free(buf);
-		return NULL;
-	}
-
-	return buf;
-}
-
-int process_file_contents(char* buf, ssize_t fsize, info_t* info)
-{
-	int i, last = 0, linecount = 0;
-
+		return (free(buf), close(fd), 0);
+	close(fd);
 	for (i = 0; i < fsize; i++)
-	{
 		if (buf[i] == '\n')
 		{
-			buf[i] = '\0';
+			buf[i] = 0;
 			build_history_list(info, buf + last, linecount++);
 			last = i + 1;
 		}
-	}
-
 	if (last != i)
 		build_history_list(info, buf + last, linecount++);
-
+	free(buf);
 	info->histcount = linecount;
-
 	while (info->histcount-- >= HIST_MAX)
 		delete_node_at_index(&(info->history), 0);
-
 	renumber_history(info);
-
-	return info->histcount;
+	return (info->histcount);
 }
+
 /**
  * build_history_list - adds entry to a history linked list
  * @info: Struct containing arguments
