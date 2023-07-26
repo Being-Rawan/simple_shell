8ff3547a6f3576e06b2d3422c530881c45eaf33f
Rawan-CS <rawanhesham8289@gmail.com>
2023-07-26 09:34:20 -0700

    updated

--- _atoi.c
+++ _atoi.c
@@ -3,43 +3,49 @@
 /**
  * interactive - returns true if shell is in interactive mode
  * @info: pointer to struct info_t
+ *
  * Return: 1 if interactive mode, 0 otherwise
  */
 int interactive(info_t *info)
 {
-	return isatty(STDIN_FILENO) && info->readfd <= STDERR_FILENO;
+	return (isatty(STDIN_FILENO) && info->readfd <= STDERR_FILENO);
 }
 
 /**
  * is_delim - checks if character is a delimiter
  * @c: the character to check
  * @delim: the delimiter string
+ *
  * Return: 1 if true, 0 otherwise
  */
-int is_delim(char c, char *delim)
+int is_delim(char c,  char *delim)
 {
 	while (*delim)
+	{
 		if (*delim++ == c)
 			return 1;
-	return 0;
+	}
+	return (0);
 }
 
 /**
  * _isalpha - checks for alphabetic character
  * @c: The character to check
+ *
  * Return: 1 if c is alphabetic, 0 otherwise
  */
 int _isalpha(int c)
 {
-	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
 
 /**
  * _atoi - converts a string to an integer
  * @s: the string to be converted
+ *
  * Return: 0 if no numbers in string, converted number otherwise
  */
-int _atoi(char *s)
+int _atoi( char *s)
 {
 	int sign = 1, output = 0;
 	unsigned int result = 0;
@@ -63,5 +69,6 @@ int _atoi(char *s)
 	else
 		output = result;
 
-	return output;
+	return (output);
 }
+
